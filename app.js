cb86c500ac2aad5e44635c4d5f8e31d5b84dd183
Yana Mishkina <mishkina_yana@mail.ru>
2022-08-02 09:21:18 +0300

    DB URL

--- app.js
+++ app.js
@@ -8,14 +8,11 @@ const router = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const allowedCors = require('./middlewares/allowedCors');
 
-const { NODE_ENV, PORT = 3000, DB_URL = 'mongodb://localhost:27017/moviesdb' } = process.env;
-
 const app = express();
 
-mongoose.connect(NODE_ENV === 'production' ? DB_URL : 'mongodb://localhost:27017/moviesdb', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const { PORT = 3000 } = process.env;
+
+mongoose.connect('mongodb://localhost:27017/moviesdb');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
